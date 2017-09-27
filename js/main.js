7216a26015b79a6adfd627bcdda9cceea3c11cce
Ilia Barashkov <ibarashkov@list.ru>
2017-09-27 09:42:26 +0300

    task 1.2

---
+++ js/main.js
@@ -0,0 +1,45 @@
+(function () {
+  const template = document.querySelector(`#templates`).content.cloneNode(true);
+  const screens = template.querySelectorAll(`.main`);
+
+  const mainScreens = [];
+  screens.forEach(function (it) {
+    if (it.classList.contains(`main--welcome`) || it.classList.contains(`main--level`)) {
+      mainScreens.push(it);
+    }
+  });
+
+  let screenNumber = 0;
+
+  const showElement = (number) => {
+    const fragment = document.createDocumentFragment();
+    const screen = document.querySelector(`.main`);
+    const mainScreen = screen.querySelector(`.main`);
+    const newScreen = fragment.appendChild(mainScreens[number]);
+
+    if (mainScreen) {
+      screen.replaceChild(newScreen, mainScreen);
+    } else {
+      screen.appendChild(newScreen);
+    }
+  };
+
+  const switchScreens = (direction) => {
+    if (direction === `right` && screenNumber < mainScreens.length - 1) {
+      showElement(++screenNumber);
+    } else if (direction === `left` && screenNumber > 0) {
+      showElement(--screenNumber);
+    }
+  };
+
+  showElement(screenNumber);
+
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 39 && evt.altKey) {
+      switchScreens(`right`);
+    } else if (evt.keyCode === 37 && evt.altKey) {
+      switchScreens(`left`);
+    }
+  });
+})();
+
