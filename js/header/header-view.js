0d7966796e6b0264293a27a55d04ceff2f07ee8a
Ilia Barashkov <ibarashkov@list.ru>
2017-11-07 01:24:50 +0300

    first try fixes

--- js/header/header-view.js
+++ js/header/header-view.js
@@ -13,20 +13,22 @@ export default class Header extends AbstractView {
     this.state = state;
   }
   get template() {
-    return `<svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
+    return `<svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0, 0, 780, 780">
        <circle
          cx="390" cy="390" r="370"
          class="timer-line"
          stroke-dasharray="${fullLength}"
          stroke-dashoffset="${fullLength / GAME_START_TIME * (GAME_START_TIME - this.state.time)}"
-         style="filter: url(.#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center" />
+         style="filter: url(.#blur);">
+       </circle>
+         </svg>
 
        <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
          <span class="timer-value-mins">${getMinutes(this.state.time)}</span>
          <span class="timer-value-dots">:</span>
          <span class="timer-value-secs">${getSeconds(this.state.time)}</span>
        </div>
-     </svg>
+     
 
      <div class="main-mistakes">
      ${new Array(this.state.state.mistakes).fill(mistake).join(``)}
